262223ef338fb810846c95d1f3cb23a4c03e3c6e
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2026-01-03 02:20:30 +0630

    feat: Burmese Agriculture Api [Complete]

---
+++ BurmeseAgriculture/BurmeseAgriculture.Api/Services/IBurmeseAgricultureService.cs
@@ -0,0 +1,12 @@
+﻿namespace BurmeseAgriculture.Api.Services
+{
+    public interface IBurmeseAgricultureService
+    {
+        AgricultureResponseDto Create(AgricultureCreateRequestDto request);
+        AgricultureResponseDto Delete(int id);
+        BurmeseAgricultureGetByIdResponseDto GetById(int id);
+        BurmeseAgricultureListResponseDto GetList(int pageNo, int pageSize);
+        AgricultureResponseDto Patch(int id, AgriculturePatchRequestDto request);
+        AgricultureResponseDto Update(int id, AgricultureUpdateRequestDto request);
+    }
+}
